7ee94d9ec8b4e88a26a4a2307669394f511146e6
Eric Finster <ericfinster@gmail.com>
2015-07-09 17:59:20 +0200

    Svg Rendering!

---
+++ opetopic-core/src/main/scala/opetopic/scratch/SvgFramework.scala
@@ -0,0 +1,17 @@
+/**
+  * SvgFramework.scala - Framework with scalatags backend
+  * 
+  * @author Eric Finster
+  * @version 0.1 
+  */
+
+package opetopic.scratch
+
+object FXSVGBundle
+    extends scalatags.Text.Cap
+    with scalatags.text.SvgTags
+    with scalatags.DataConverters
+    with scalatags.Text.Aggregate{ object svgattr extends scalatags.Text.Cap with scalatags.Text.SvgAttrs }
+
+import FXSVGBundle._
+
